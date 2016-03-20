09c1cb3e14aa017a259f46f7b794f0a8cbc2f20c
kingsley ijomah <kingsley.ijomah@gmail.com>
2016-03-20 22:14:00 +0000

    chage

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'contactbook'
+set :repo_url, 'git@github.com:kingsley-ijomah/contactbook.git'
 
 set :branch, :master
 set :deploy_to, '/home/deploy/contactbook'
